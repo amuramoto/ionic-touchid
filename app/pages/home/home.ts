010d31162ea19976fe16a3074270acb9da041121
Alex Muramoto <amuramoto@apache.org>
2016-08-03 22:48:55 -0700

    Starter app

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,35 +1,13 @@
 import {Component} from '@angular/core';
-import {NavController, Platform} from 'ionic-angular';
-import {TouchID} from 'ionic-native';
-import {LoggedinPage} from '../loggedin/loggedin';
+import {NavController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
 
-	private touchIdAvailable: boolean;
-
-  constructor(public _navCtrl: NavController, private _platform: Platform) {
+  constructor(public _navCtrl: NavController) {
 
   }
 
-  ionViewLoaded () {
-  	this._platform.ready().then(()=>{
-	  	TouchID.isAvailable().then(
-		    res => this.touchIdAvailable = true,
-		    err => this.touchIdAvailable = false
-	  	);
-
-		
-	});
-  }
-
-  private startTouchID () {
-  	TouchID.verifyFingerprintWithCustomPasswordFallback('Scan your fingerprint please')
-		  .then(
-		    res => this._navCtrl.push(LoggedinPage),
-		    err => console.error('Error', err)
-		  );
-  }
 }
